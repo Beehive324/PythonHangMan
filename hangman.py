7d3aa4d4a4d21d21e77bcfb2a0c0e58485b4e6e5
Fairson Soares <Fairson123@hotmail.com>
2023-11-30 12:02:55 +0000

    multiple inputs

--- hangman.py
+++ hangman.py
@@ -1,13 +1,28 @@
 import random
 
-print("H A N G M A N")
+attempts = 8
+print("H A N G M A N " ,"#" ,attempts, "attempts")
 #print("The game will be available soon", end = ".")
 words = ["python","java","swift","javascript"]
 word = random.choice(words)
 
-user_input = str(input("Guess the word:" + ('-' * len(word)).replace('---', word[:3], 1)))
+word_list = list(word)
+guessed_word = ''
 
-if user_input == word:
-    print("You survived!")
-else:
-    print("You lost!")
+for _ in range(1,1000): #amount of tries
+    user_input = str(input("Input a letter:" + ('-' * len(word))))
+    if user_input in word_list:
+        attempts -= 1
+        guessed_word += user_input
+        print(guessed_word,"#" ,attempts, "attempts")
+        if attempts == 0:
+             print("Thanks for playing!")
+             exit()
+    elif user_input not in word_list:
+        attempts -= 1
+        print("That letter doesn't appear in the word.","#" ,attempts, "attempts")
+        if attempts == 0:
+            print("Thanks for playing!")
+            exit()
+
+
