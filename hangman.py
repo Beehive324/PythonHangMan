5b7311d45a9021bdb663602a37f46e9cf88f3ef4
Fairson Soares <Fairson123@hotmail.com>
2023-11-30 11:03:11 +0000

    Guess the word functionality

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@ print("H A N G M A N")
 words = ["python","java","swift","javascript"]
 word = random.choice(words)
 
-user_input = str(input("Guess the word:"))
+user_input = str(input("Guess the word:" + ('-' * len(word)).replace('---', word[:3], 1)))
 
 if user_input == word:
     print("You survived!")
