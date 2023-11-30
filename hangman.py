5234417138581cb6c3ba89144abc9970ab2b8f80
Fairson Soares <Fairson123@hotmail.com>
2023-11-30 13:39:28 +0000

    guessing a word functionality

--- hangman.py
+++ hangman.py
@@ -5,20 +5,25 @@ print("H A N G M A N " ,"#" ,attempts, "attempts")
 #print("The game will be available soon", end = ".")
 words = ["python","java","swift","javascript"]
 word = random.choice(words)
-
+guess_input = ('_') * len(word)
+display = []
+for i in word:
+    display += "_"
 word_list = list(word)
 guessed_word = ''
 
-for _ in range(1,1000): #amount of tries
-    user_input = str(input("Input a letter:" + ('-' * len(word))))
-    if user_input in word_list:
-        attempts -= 1
-        guessed_word += user_input
-        print(guessed_word,"#" ,attempts, "attempts")
-        if attempts == 0:
-             print("Thanks for playing!")
-             exit()
-    elif user_input not in word_list:
+for _ in range(1, 1000):
+    user_input = str(input("Input a letter:" + guess_input))
+    for position in range(len(word)):
+        if user_input == word[position]:
+            attempts -= 1
+            display[position]=word[position]
+            if attempts == 0:
+                print("Thanks for playing!")
+                exit()
+    print(*display,"#" ,attempts, "attempts")
+   
+    if user_input not in word_list:
         attempts -= 1
         print("That letter doesn't appear in the word.","#" ,attempts, "attempts")
         if attempts == 0:
